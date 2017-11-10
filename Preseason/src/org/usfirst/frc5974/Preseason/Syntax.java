54a30e7688982942647b5b4f117b1207c2a4906f
5974 Developer <clavierkid@gmail.com>
2017-11-10 12:21:22 -0700

    Pushing robot code

---
+++ Preseason/src/org/usfirst/frc5974/Preseason/Syntax.java
@@ -0,0 +1,10 @@
+package org.usfirst.frc5974.Preseason;
+
+public class Syntax {
+	public static void Body(){
+	String hello = "Hello World";
+	for(int x = 2; x<5; x=x+2){
+		System.out.println(x);
+	}
+}
+}
