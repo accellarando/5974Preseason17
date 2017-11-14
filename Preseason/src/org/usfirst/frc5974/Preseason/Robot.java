261e240477eb3e25ece4ea882a622aeed18b0cbf
JavaGreenhorn <MaxKosn@gmail.com>
2017-11-14 13:28:55 -0700

    Function wizardry
    OOOoooOooOoooo

--- Preseason/src/org/usfirst/frc5974/Preseason/Robot.java
+++ Preseason/src/org/usfirst/frc5974/Preseason/Robot.java
@@ -329,27 +329,30 @@ public class Robot extends IterativeRobot {
     /**
      * This function is called periodically during operator control
      */
+    
+    public void twitchTurn(int turnTo) { 
+		if (turnTo < 0) {
+			lFront.set(-1);
+        	lBack.set(-1);
+        	rFront.set(-1);
+        	rBack.set(-1);
+		}
+		if (turnTo > 1) {
+			lFront.set(1);
+        	lBack.set(1);
+        	rFront.set(1);
+        	rBack.set(1);
+		}
+		Timer.delay(0.3);
+		lFront.set(0);
+    	lBack.set(0);
+    	rFront.set(0);
+    	rBack.set(0);
+	}
+    
     public void teleopPeriodic() {
     	updateAll();
-    	int twitchTurn (int turnTo) { //possible bool? Might be constantly used, may not.
-    		if (turnTo < 0) {
-    			lFront.set(-1);
-            	lBack.set(-1);
-            	rFront.set(-1);
-            	rBack.set(-1);
-    		}
-    		if (turnTo > 1) {
-    			lFront.set(1);
-            	lBack.set(1);
-            	rFront.set(1);
-            	rBack.set(1);
-    		}
-    		Timer.delay(0.3);
-    		lFront.set(0);
-        	lBack.set(0);
-        	rFront.set(0);
-        	rBack.set(0);
-    	}
+    	
     	
     	//Drive speed switcher
     	if (ButtonB && !fast){
